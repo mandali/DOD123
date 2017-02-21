7f75052ad436dbfb3705776c9265e5604fdaf04e
Vidhyadhar <svidyadhar.neti@omniwyse.com>
2017-02-21 11:33:25 +0530

    updated services

--- Discounts/src/main/java/com/omniwyse/dod/controller/DODController.java
+++ Discounts/src/main/java/com/omniwyse/dod/controller/DODController.java
@@ -469,7 +469,7 @@ public class DODController {
 		
 		}
 		catch(Exception exception){
-			//logger.error("Exception in "+METHOD_NAME+""+exception.getMessage());
+			logger.error("Exception in "+METHOD_NAME+""+exception.getMessage());
 		}
 		return responseEntity;
 		
