66b16f72dc06712f233a93096c998bc66bb2d341
Vidhyadhar <svidyadhar.neti@omniwyse.com>
2017-03-02 15:02:06 +0530

    adding model classes

--- Discounts/src/main/java/com/omniwyse/dod/model/Beacon.java
+++ Discounts/src/main/java/com/omniwyse/dod/model/Beacon.java
@@ -25,5 +25,48 @@ public class Beacon implements java.io.Serializable{
 	private String beaconStatus;
 	@Column(name = "BRD_CREATED")
 	private Timestamp created;
+	
+	public Beacon(){
+	}
+	
+	
+	
+	
+	public Beacon(Long beaconId, String beaconName, String beaconStatus, Timestamp created) {
+		this.beaconId = beaconId;
+		this.beaconName = beaconName;
+		this.beaconStatus = beaconStatus;
+		this.created = created;
+	}
+
+
+
+
+	public Long getBeaconId() {
+		return beaconId;
+	}
+	public void setBeaconId(Long beaconId) {
+		this.beaconId = beaconId;
+	}
+	public String getBeaconName() {
+		return beaconName;
+	}
+	public void setBeaconName(String beaconName) {
+		this.beaconName = beaconName;
+	}
+	public String getBeaconStatus() {
+		return beaconStatus;
+	}
+	public void setBeaconStatus(String beaconStatus) {
+		this.beaconStatus = beaconStatus;
+	}
+	public Timestamp getCreated() {
+		return created;
+	}
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
+	
+	
 
 }
