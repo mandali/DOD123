6d3bbe9fe85237c601ec0169c7a91b801ba890a6
Vidhyadhar <svidyadhar.neti@omniwyse.com>
2017-03-02 15:22:09 +0530

    updating services

---
+++ Discounts/src/main/java/com/omniwyse/dod/model/MerchantBeacon.java
@@ -0,0 +1,70 @@
+package com.omniwyse.dod.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class MerchantBeacon implements java.io.Serializable
+
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "BC_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long merchantBeaconId;
+	@OneToOne
+	@JoinColumn(name="B_ID")
+	private Beacon beaconId;
+	@OneToOne
+	@JoinColumn(name="M_ID", referencedColumnName="ID")
+	private MerchantProfile merchantProfile;
+	
+	public MerchantBeacon(){
+	}
+	
+	
+
+	public MerchantBeacon(Long merchantBeaconId, Beacon beaconId, MerchantProfile merchantProfile) {
+		this.merchantBeaconId = merchantBeaconId;
+		this.beaconId = beaconId;
+		this.merchantProfile = merchantProfile;
+	}
+
+
+
+	public Long getMerchantBeaconId() {
+		return merchantBeaconId;
+	}
+
+	public void setMerchantBeaconId(Long merchantBeaconId) {
+		this.merchantBeaconId = merchantBeaconId;
+	}
+
+	public Beacon getBeaconId() {
+		return beaconId;
+	}
+
+	public void setBeaconId(Beacon beaconId) {
+		this.beaconId = beaconId;
+	}
+
+	public MerchantProfile getMerchantProfile() {
+		return merchantProfile;
+	}
+
+	public void setMerchantProfile(MerchantProfile merchantProfile) {
+		this.merchantProfile = merchantProfile;
+	}
+	
+	
+	
+
+}
